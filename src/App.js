433bdd987938082c8173ee8fae698e231ea7e662
Stephanie Mendoza <aniedoza@gmail.com>
2024-07-22 01:51:19 +0800

    Applied router.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Home} from './home';
+import {AboutMe} from './aboutme';
+import {Portfolio} from './portfolio';
+import {TechStack} from './techstack';
+import {SoftSkills} from './softskills';
+import {Education} from './education';
+import {Certifications} from './certifications';
+import {Contact} from './contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/tech-stack" element={<TechStack />} />
+        <Route path="/soft-skills" element={<SoftSkills />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/certifications" element={<Certifications />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
